b0890c311014535feee78d820dafbe8badda89c3
qin <775051675@qq.com>
2017-06-28 00:44:10 +0800

    添加满屏启动页

---
+++ app/src/main/java/com/example/administrator/jkbd/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.administrator.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        mCountDowntime.start();
+    }
+    CountDownTimer mCountDowntime=new CountDownTimer(4000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+}
