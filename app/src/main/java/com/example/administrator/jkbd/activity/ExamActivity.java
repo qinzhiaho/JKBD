d580f50186600fb51b36d96c67f8687c0d4b7a95
qin <775051675@qq.com>
2017-06-30 00:42:48 +0800

    设置驾考题库页面

---
+++ app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.jkbd.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.jkbd.R;
+
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exam);
+    }
+
+}
