e2e422617e5e47c3ab48e806040e150362800bce
Administrator <775051675@qq.com>
2017-06-30 10:35:05 +0800

    添加主页面到驾考页面的跳转和退出app功能

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.administrator.jkbd.R;
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+    public void exit(View view) {
+        finish();
     }
 }
